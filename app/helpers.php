c63115fe5511c652739a06538b53b54fb21473d7
HosseinEsteki <45480012+HosseinEsteki@users.noreply.github.com>
2023-10-06 20:52:42 +0330

    add slug function to helpers.php

--- app/helpers.php
+++ app/helpers.php
@@ -1,6 +1,12 @@
 <?php
+namespace App;
+use Illuminate\Support\Str;
+
 require_once 'enums.php';
 function message()
 {
     return new \App\Message();
+}
+function slug($value){
+    return Str::replace(' ','-',$value);
 }
